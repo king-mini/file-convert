66f7a909989391de5384e326d38428826e7baf76
psh041283 <psh041283@gmail.com>
2025-11-26 05:07:42 +0900

    Refactor MergePdf component: remove unused useEffect import to streamline code

--- src/pages/MergePdf.tsx
+++ src/pages/MergePdf.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect } from 'react';
+import { useState, useCallback } from 'react';
 import { mergePdfs, getPageCount } from '../utils/pdfMerger';
 import type { PdfFile, MergeProgress } from '../utils/pdfMerger';
 import './MergePdf.css';
